e90c9044b65fe1d7452514eaee0cee0efb68042e
shivagarg1310 <shivagarg1310@gmail.com>
2020-12-30 17:31:33 +0530

    my first commit

---
+++ arthematic operator.c
@@ -0,0 +1,13 @@
+/* code to demonstrate the working of arthematic operator*/
+
+#include<stdio.h> 
+
+void main()
+{
+int a = 15, b =6, result ;
+result = a+b;
+printf("sum of %d and %d is %d" ,a ,b, result);
+printf("\n");
+}
+
+
